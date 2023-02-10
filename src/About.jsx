38391ac3008917541ada6253744771d555917750
Laxman kendre <106816249+lkendre5652@users.noreply.github.com>
2023-02-10 16:18:47 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () =>{
+    return(<>
+        About Page 
+        About Page 
+        About Page 
+        About Page 
+        About Page 
+        About Page 
+    </>)
+}
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Home = () =>{
+    return(<>
+        Home Page 
+        Home Page 
+        Home Page 
+        Home Page 
+        Home Page 
+        Home Page 
+    </>)
+}
